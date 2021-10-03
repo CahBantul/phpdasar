5a89f674ef929eb3197e0b642abe04b06815344e
Fardan Nozami <fardan.nozami@gmail.com>
2021-10-03 22:56:15 +0700

    pertemuan 1 dan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,35 @@
+<?php 
+// pertemuan ke 2
+// Sintaks PHP
+
+// standar output
+// echo, print
+// print_r
+// var_dump
+
+// echo "Fardan Nozami Ajitama";
+// print "Fardan Nozami Ajitama";
+// print_r("Fardan Nozami Ajitama");
+// var_dump("Fardan Nozami Ajitama");
+
+// penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTMLdid alam PHP
+$nama = "Nozami";
+$x = 1;
+$x = 5;
+echo $x;
+// echo $z;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Belajar PHP</title>
+</head>
+<body>
+  <h1>Halo, Selamat Datang <?php echo $nama; ?></h1>
+</body>
+</html>
