39ba8ae1449b884fe337d66593ea923130772899
Cah Bantul <fardan.nozami@gmail.com>
2022-01-26 15:45:14 +0700

    menit 26

---
+++ pertemuan7/latian2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Kontak</title>
+</head>
+<body>
+  <ul>
+    <li><img src="img/<?= $_GET["gambar"]; ?>" alt=""></li>
+    <li><?= $_GET["nama"]; ?> </li>
+    <li><?= $_GET["alamat"]; ?> </li>
+    <li><?= $_GET["noHP"]; ?> </li>
+    <li><?= $_GET["email"]; ?> </li>
+  </ul>
+</body>
+</html>
