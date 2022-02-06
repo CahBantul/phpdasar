96a258ec560da21243306da612f360f098e66ea0
Fardan Nozami <fardan.nozami@gmail.com>
2022-02-06 23:04:38 +0700

    perteuan 11 done

---
+++ pertemuan11/functions.php
@@ -0,0 +1,59 @@
+<?php 
+// koneksi ke database
+$db = mysqli_connect("localhost", "root", "", "phpdasar");
+
+function query($query){
+  global $db;
+  $result = mysqli_query($db, $query);
+  $rows = [];
+  while($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
+
+// tambah data
+function tambah($data){
+  global $db;
+
+  $nama = htmlspecialchars($data["nama"]);
+  $email = htmlspecialchars($data["email"]);
+  $gambar = htmlspecialchars($data["gambar"]);
+  $alamat = htmlspecialchars($data["alamat"]);
+  $noHP = htmlspecialchars($data["noHP"]);
+
+  $query = "INSERT INTO kontak VALUES ('', '$nama', '$alamat', '$email', '$noHP', '$gambar')";
+
+  mysqli_query($db, $query);
+
+  return mysqli_affected_rows($db);
+}
+
+// hapus data
+function hapus($id) {
+  global $db;
+  mysqli_query($db, "DELETE FROM kontak WHERE id=$id");
+
+  return mysqli_affected_rows($db);
+}
+
+// ubah kontak
+function ubah($data){
+  global $db;
+
+  $id = $data["id"];
+  $nama = htmlspecialchars($data["nama"]);
+  $email = htmlspecialchars($data["email"]);
+  $gambar = htmlspecialchars($data["gambar"]);
+  $alamat = htmlspecialchars($data["alamat"]);
+  $noHP = htmlspecialchars($data["noHP"]);
+  echo($gambar);
+
+  $query = "UPDATE kontak SET nama ='$nama', alamat='$alamat', email='$email', noHP='$noHP', gambar='$gambar' WHERE id= $id";
+
+  mysqli_query($db, $query);
+
+  return mysqli_affected_rows($db);
+}
+?>
---
+++ pertemuan11/index.php
@@ -0,0 +1,50 @@
+<?php 
+require "functions.php";
+$kontak = query("SELECT * FROM kontak");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Halaman Admin</title>
+</head>
+<body>
+  <h1>Daftar Kontak</h1>
+  <a href="tambah.php">tambah kontak</a>
+  <br><br>
+<table border="1" cellpadding="10" cellspacing="0">
+  <tr>
+    <th>No</th>
+    <th>Aksi</th>
+    <th>Foto</th>
+    <th>Nama</th>
+    <th>No HP</th>
+    <th>Alamat</th>
+    <th>Email</th>
+  </tr>
+  <?php $i=1; ?>
+  <?php foreach ($kontak as $row) :?>
+    
+  <tr>
+    <td><?= $i; ?></td>
+    <td>
+      <a href="ubah.php?id=<?= $row["id"];?>">ubah</a> |
+      <a href="hapus.php?id=<?= $row["id"];?>" onclick="return confirm('yakin?');">hapus</a>
+    </td>
+    <td>
+      <img src="img/<?= $row["gambar"];?>" width="60" >
+    </td>
+    <td><?= $row["nama"]; ?></td>
+    <td><?= $row["noHP"]; ?></td>
+    <td><?= $row["alamat"]; ?></td>
+    <td><?= $row["email"]; ?></td>
+    <?php $i++; ?>
+  </tr>
+  <?php endforeach; ?>
+
+</table>
+</body>
+</html>
