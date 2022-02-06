52ac3a59b1234b9f1e150dd392fc25742921c7e8
Fardan Nozami <fardan.nozami@gmail.com>
2022-02-06 16:06:28 +0700

    Pertemuan 7 done

--- pertemuan7/latian2.php
+++ pertemuan7/latian2.php
@@ -1,18 +1,20 @@
 <?php 
 if (!isset($_GET["nama"])||!isset($_GET["gambar"])|| !isset($_GET["email"])|| !isset($_GET["alamat"]) || !isset($_GET["noHP"])) {
   // redirect
-  header("Location: latian1.php");
+        header("Location: latian1.php");
   exit;
 };
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Detail Kontak</title>
 </head>
+
 <body>
   <ul>
     <li><img src="img/<?= $_GET["gambar"]; ?>" alt=""></li>
@@ -23,4 +25,5 @@ if (!isset($_GET["nama"])||!isset($_GET["gambar"])|| !isset($_GET["email"])|| !i
   </ul>
   <a href="latian1.php">kembali ke halaman kontak</a>
 </body>
+
 </html>
